fb379a56fe8c923b8fe3b6a0e12cd1cb41ff3e9b
woongs <woongs.rich@kakaocorp.com>
2022-06-20 15:24:52 +0900

    Create Project

---
+++ Swift-Chessgame/Swift-Chessgame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-Chessgame
+//
+//  Copyright (c) 2022 woongs All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
