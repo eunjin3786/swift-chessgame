cdbf3c264aeefdcef464b2112b14706be71dd6da
woongs <woongs.rich@kakaocorp.com>
2022-06-20 18:26:05 +0900

    주석 추가

--- Swift-Chessgame/Swift-ChessgameTests/BoardTests.swift
+++ Swift-Chessgame/Swift-ChessgameTests/BoardTests.swift
@@ -18,12 +18,15 @@ class BoardTests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
     
     func test_empty_pieces_when_init_board() {
+        // given
         let emptyRanks = [3, 4, 5, 6]
         let files = "ABCDEFGH"
+        
+        // then
         for rank in emptyRanks {
             for file in files {
                 let piece = self.sut.findPiece(at: Position(rank: rank, file: String(file)))
@@ -33,8 +36,11 @@ class BoardTests: XCTestCase {
     }
     
     func test_pawn_when_init_board() {
+        // given
         let pawnRanks = [2, 7]
         let files = "ABCDEFGH"
+        
+        // then
         for rank in pawnRanks {
             for file in files {
                 let piece = self.sut.findPiece(at: Position(rank: rank, file: String(file)))
@@ -44,6 +50,7 @@ class BoardTests: XCTestCase {
     }
     
     func test_luke_when_init_board() {
+        // given
         let whiteLukes = [
             self.sut.findPiece(at: Position(rank: 1, file: "A")),
             self.sut.findPiece(at: Position(rank: 1, file: "H")),
@@ -53,6 +60,7 @@ class BoardTests: XCTestCase {
             self.sut.findPiece(at: Position(rank: 8, file: "H")),
         ]
         
+        // then
         whiteLukes.forEach { piece in
             let luke = piece as? Luke
             XCTAssertNotNil(luke)
@@ -67,6 +75,7 @@ class BoardTests: XCTestCase {
     }
     
     func test_knight_when_init_board() {
+        // given
         let whiteKnights = [
             self.sut.findPiece(at: Position(rank: 1, file: "B")),
             self.sut.findPiece(at: Position(rank: 1, file: "G")),
@@ -76,6 +85,7 @@ class BoardTests: XCTestCase {
             self.sut.findPiece(at: Position(rank: 8, file: "G")),
         ]
         
+        // then
         whiteKnights.forEach { piece in
             let knight = piece as? Knight
             XCTAssertNotNil(knight)
@@ -90,6 +100,7 @@ class BoardTests: XCTestCase {
     }
     
     func test_bishop_when_init_board() {
+        // given
         let whiteBishops = [
             self.sut.findPiece(at: Position(rank: 1, file: "C")),
             self.sut.findPiece(at: Position(rank: 1, file: "F")),
@@ -99,6 +110,7 @@ class BoardTests: XCTestCase {
             self.sut.findPiece(at: Position(rank: 8, file: "F")),
         ]
         
+        // then
         whiteBishops.forEach { piece in
             let bishop = piece as? Bishop
             XCTAssertNotNil(bishop)
@@ -113,9 +125,11 @@ class BoardTests: XCTestCase {
     }
     
     func test_queen_when_init_board() {
+        // given
         let whiteQueen = self.sut.findPiece(at: Position(rank: 1, file: "E"))
         let blackQueen = self.sut.findPiece(at: Position(rank: 8, file: "E"))
         
+        // then
         XCTAssertNotNil(whiteQueen as? Queen)
         XCTAssertEqual(whiteQueen?.team, .white)
         
