20a6a86d0695a33ac14232a3d2ee0780e1ea90c1
hayden.chae <hayden.chae@kakaocorp.com>
2022-06-20 16:31:47 +0900

    프로젝트 초기화

---
+++ ChessGame/ChessGame/ChessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessGameApp.swift
+//  ChessGame
+//
+//  Created by kakao on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct ChessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
