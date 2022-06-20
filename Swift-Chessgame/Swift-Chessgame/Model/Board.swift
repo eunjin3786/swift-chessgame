75e376702557b6c35bed2c91832ad0a7f8ae1ae7
woongs <woongs.rich@kakaocorp.com>
2022-06-20 18:00:36 +0900

    board, piece 관련 객체들 생성

--- Swift-Chessgame/Swift-Chessgame/Model/Board.swift
+++ Swift-Chessgame/Swift-Chessgame/Model/Board.swift
@@ -7,3 +7,55 @@
 
 
 import Foundation
+
+protocol PieceType {
+    
+    var position: Position { get }
+    var team: Team { get }
+}
+
+struct Position {
+    
+    var rank: Int
+    var file: String
+}
+
+enum Team {
+    
+    case black
+    case white
+}
+
+struct Pawn: PieceType {
+    
+    var position: Position
+    let team: Team
+}
+
+struct Knight: PieceType {
+    
+    var position: Position
+    let team: Team
+}
+
+struct Bishop: PieceType {
+    
+    var position: Position
+    let team: Team
+}
+
+struct Luke: PieceType {
+    
+    var position: Position
+    let team: Team
+}
+
+struct Queen: PieceType {
+    
+    var position: Position
+    let team: Team
+}
+
+class Board {
+    
+}
