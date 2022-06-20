77348cb54d47bdce011b5bc9fa7b880d6dec8eb2
woongs <woongs.rich@kakaocorp.com>
2022-06-20 18:24:25 +0900

    luke, knight, bishop, queen 초기화 테스트 추가

--- Swift-Chessgame/Swift-ChessgameTests/BoardTests.swift
+++ Swift-Chessgame/Swift-ChessgameTests/BoardTests.swift
@@ -42,4 +42,84 @@ class BoardTests: XCTestCase {
             }
         }
     }
+    
+    func test_luke_when_init_board() {
+        let whiteLukes = [
+            self.sut.findPiece(at: Position(rank: 1, file: "A")),
+            self.sut.findPiece(at: Position(rank: 1, file: "H")),
+        ]
+        let blackLukes = [
+            self.sut.findPiece(at: Position(rank: 8, file: "A")),
+            self.sut.findPiece(at: Position(rank: 8, file: "H")),
+        ]
+        
+        whiteLukes.forEach { piece in
+            let luke = piece as? Luke
+            XCTAssertNotNil(luke)
+            XCTAssertEqual(luke?.team, .white)
+        }
+        
+        blackLukes.forEach { piece in
+            let luke = piece as? Luke
+            XCTAssertNotNil(luke)
+            XCTAssertEqual(luke?.team, .black)
+        }
+    }
+    
+    func test_knight_when_init_board() {
+        let whiteKnights = [
+            self.sut.findPiece(at: Position(rank: 1, file: "B")),
+            self.sut.findPiece(at: Position(rank: 1, file: "G")),
+        ]
+        let blackKnights = [
+            self.sut.findPiece(at: Position(rank: 8, file: "B")),
+            self.sut.findPiece(at: Position(rank: 8, file: "G")),
+        ]
+        
+        whiteKnights.forEach { piece in
+            let knight = piece as? Knight
+            XCTAssertNotNil(knight)
+            XCTAssertEqual(knight?.team, .white)
+        }
+        
+        blackKnights.forEach { piece in
+            let knight = piece as? Knight
+            XCTAssertNotNil(knight)
+            XCTAssertEqual(knight?.team, .black)
+        }
+    }
+    
+    func test_bishop_when_init_board() {
+        let whiteBishops = [
+            self.sut.findPiece(at: Position(rank: 1, file: "C")),
+            self.sut.findPiece(at: Position(rank: 1, file: "F")),
+        ]
+        let blackBishops = [
+            self.sut.findPiece(at: Position(rank: 8, file: "C")),
+            self.sut.findPiece(at: Position(rank: 8, file: "F")),
+        ]
+        
+        whiteBishops.forEach { piece in
+            let bishop = piece as? Bishop
+            XCTAssertNotNil(bishop)
+            XCTAssertEqual(bishop?.team, .white)
+        }
+        
+        blackBishops.forEach { piece in
+            let bishop = piece as? Bishop
+            XCTAssertNotNil(bishop)
+            XCTAssertEqual(bishop?.team, .black)
+        }
+    }
+    
+    func test_queen_when_init_board() {
+        let whiteQueen = self.sut.findPiece(at: Position(rank: 1, file: "E"))
+        let blackQueen = self.sut.findPiece(at: Position(rank: 8, file: "E"))
+        
+        XCTAssertNotNil(whiteQueen as? Queen)
+        XCTAssertEqual(whiteQueen?.team, .white)
+        
+        XCTAssertNotNil(blackQueen as? Queen)
+        XCTAssertEqual(blackQueen?.team, .black)
+    }
 }
