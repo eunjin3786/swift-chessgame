88bd04d64e18c9a82d386e32fac489041e40947f
eugeneShin-dev <shinyou@kakao.com>
2022-06-21 16:16:19 +0900

    [체스게임] [Step1] 프로젝트 생성

---
+++ Chess/Chess/Chess.swift
@@ -0,0 +1,8 @@
+//
+//  Chess.swift
+//  Chess
+//
+//  Created by Hailey on 2022/06/20.
+//
+
+import Foundation
---
+++ Chess/Chess/Piece.swift
@@ -0,0 +1,8 @@
+//
+//  Piece.swift
+//  Chess
+//
+//  Created by Hailey on 2022/06/20.
+//
+
+import Foundation
